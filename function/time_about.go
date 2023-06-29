e2d10871bc215275238ae39112707bb7ac3f6707
DELL <2509144896@qq.com>
2023-06-29 11:21:19 +0800

    修改基础模型

--- function/time_about.go
+++ function/time_about.go
@@ -1,12 +1,28 @@
 package function
 
-import "time"
+import (
+	"time"
+)
 
-func GetTimeStr() string {
+func GetCurrentTimeStr() string {
 	return time.Now().Format("2006-01-02 15:04:05")
 }
 
-// GetTimeUnix 获取当前时间戳
-//func GetTimeUnix() int64 {
+// GetCurrentTimeUnix 获取当前时间戳
+//func GetCurrentTimeUnix() int64 {
 //	return time.Now().Unix()
 //}
+
+func ParseDateStrToLocTime(timeStr string) time.Time {
+	loc, err := time.LoadLocation("Asia/Shanghai")
+	if err != nil {
+		return time.Time{}
+	}
+
+	timeObj, err := time.ParseInLocation("2006-01-02", timeStr, loc)
+	if err != nil {
+		return time.Time{}
+	}
+
+	return timeObj
+}
