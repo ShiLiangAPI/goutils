d582abeaa2aa11af48d223b3c2b8a1508e938ba3
reader <2509144896@qq.com>
2023-09-03 21:39:06 +0800

    [Add] 增加标签处理

---
+++ function/tag_handler.go
@@ -0,0 +1,67 @@
+package function
+
+import (
+	"github.com/go-playground/validator/v10"
+	"reflect"
+	"strings"
+)
+
+// GetStructMsg 返回结构体中的msg参数
+func GetStructMsg(err error, obj any) string {
+	// 使用的时候，需要传obj的指针
+	getObj := reflect.TypeOf(obj)
+	// 将err接口断言为具体类型
+	if errs, ok := err.(validator.ValidationErrors); ok {
+		// 断言成功
+		for _, e := range errs {
+			// 循环每一个错误信息
+			// 根据报错字段名，获取结构体的具体字段
+			if f, exits := getObj.Elem().FieldByName(e.Field()); exits {
+				msg := f.Tag.Get("msg")
+				if msg != "" {
+					if strings.Contains(msg, ";") {
+						msgList := strings.Split(msg, ";")
+						for _, msgValue := range msgList {
+							keyValueList := strings.SplitN(msgValue, ":", 1)
+							if keyValueList[0] == e.Tag() {
+								return keyValueList[1]
+							}
+						}
+					} else {
+						if strings.Contains(msg, ":") {
+							keyValueList := strings.SplitN(msg, ":", 1)
+							return keyValueList[1]
+						}
+						return msg
+					}
+				}
+				gorm := f.Tag.Get("gorm")
+				if gorm != "" {
+					gormMsgList := strings.Split(gorm, ";")
+					for _, gormMsg := range gormMsgList {
+						keyValueList := strings.SplitN(gormMsg, ":", 1)
+						if keyValueList[0] == "comment" {
+							return keyValueList[1] + "不能为空"
+						}
+					}
+				}
+			}
+		}
+	}
+
+	return err.Error()
+}
+
+//// GetValidComment 返回结构体中的comment参数
+//func GetValidComment(err error, obj any) string {
+//
+//	getObj := reflect.TypeOf(obj)
+//	if f, exits := getObj.Elem().FieldByName(err.Field()); exits {
+//		tagValue := f.Tag.Get("gorm")
+//		reg := regexp.MustCompile(`comment:'(.*?)'`)
+//		matchArr := reg.FindStringSubmatch(tagValue)
+//		return matchArr[len(matchArr)-1]
+//	}
+//
+//	return ""
+//}
--- go.mod
+++ go.mod
@@ -3,12 +3,20 @@ module github.com/ShiLiangAPI/goutils
 go 1.19
 
 require (
+	github.com/go-playground/validator/v10 v10.15.3
 	golang.org/x/crypto v0.12.0
 	gorm.io/gorm v1.25.1
 	gorm.io/plugin/soft_delete v1.2.1
 )
 
 require (
+	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/leodido/go-urn v1.2.4 // indirect
+	golang.org/x/net v0.10.0 // indirect
+	golang.org/x/sys v0.11.0 // indirect
+	golang.org/x/text v0.12.0 // indirect
 )
