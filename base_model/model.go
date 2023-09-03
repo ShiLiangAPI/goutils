1fe748232bc444684f413d8d1bb1e59458349efd
reader <2509144896@qq.com>
2023-09-03 19:17:58 +0800

    [add] 增加模型提示

--- base_model/model.go
+++ base_model/model.go
@@ -8,9 +8,9 @@ import (
 )
 
 type Model struct {
-	ID        string           `gorm:"<-:create;type:bigint;primaryKey" json:"id"`
-	CreatedAt *types.LocalTime `gorm:"comment:'创建时间'" json:"created_at"`
-	UpdatedAt *types.LocalTime `gorm:"comment:'修改时间'" json:"updated_at"`
+	ID        string           `gorm:"<-:create;type:bigint;primaryKey" json:"id"` // id
+	CreatedAt *types.LocalTime `gorm:"comment:'创建时间'" json:"created_at"`           // 创建时间
+	UpdatedAt *types.LocalTime `gorm:"comment:'修改时间'" json:"updated_at"`           // 修改时间
 }
 
 func (obj *Model) BeforeCreate(tx *gorm.DB) (err error) {
