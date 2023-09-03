11526a094fd9929ea391b803329a4eeb4eb825ed
reader <2509144896@qq.com>
2023-09-04 01:56:20 +0800

    [Mod] 增加filter标签处理

--- function/struct_to_map.go
+++ function/struct_to_map.go
@@ -20,6 +20,38 @@ func StructToMap(st any) map[string]interface{} {
 	for i := 0; i < relType.NumField(); i++ {
 		name := relType.Field(i).Name
 		tag := relType.Field(i).Tag.Get("json")
+		if ValueInSlice[string]([]string{"-", "created_at", "updated_at", "delete_at"}, tag) {
+			continue
+		}
+		if tag != "" {
+			index := strings.Index(tag, ",")
+			if index == -1 {
+				name = tag
+			} else {
+				name = tag[:index]
+			}
+		}
+		m[name] = val.Field(i).Interface()
+	}
+	return m
+}
+
+// StructToFilterMap 使用反射实现，完美地兼容了field标签的处理
+func StructToFilterMap(st any) map[string]interface{} {
+	m := make(map[string]interface{})
+	val := reflect.ValueOf(st)
+	if val.Kind() == reflect.Ptr {
+		val = val.Elem()
+	}
+	if val.Kind() != reflect.Struct {
+		return m
+	}
+
+	relType := val.Type()
+	for i := 0; i < relType.NumField(); i++ {
+		name := relType.Field(i).Name
+		tag := relType.Field(i).Tag.Get("field")
+
 		if tag != "" {
 			index := strings.Index(tag, ",")
 			if index == -1 {
