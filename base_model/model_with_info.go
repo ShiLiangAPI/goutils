8f5515458d479ac68facbf8f8d50f5cc49767127
reader <2509144896@qq.com>
2023-09-18 02:33:43 +0800

    [Mod] 优化方法

--- base_model/model_with_info.go
+++ base_model/model_with_info.go
@@ -13,7 +13,7 @@ type BaseModelInfo struct {
 	ID        string                `gorm:"<-:create;type:bigint;primaryKey" json:"id"`                            // id
 	CreatedAt *types.LocalTime      `gorm:"comment:'创建时间'" json:"created_at"`                                      // 创建时间
 	UpdatedAt *types.LocalTime      `gorm:"comment:'修改时间'" json:"updated_at"`                                      // 修改时间
-	DeletedAt soft_delete.DeletedAt `gorm:"<-:create;index;uniqueIndex:udx_1" json:"-"`                            // 删除时间
+	DeletedAt soft_delete.DeletedAt `gorm:"<-:create;softDelete:milli;index;uniqueIndex:udx_1" json:"-"`           // 删除时间
 	Name      string                `gorm:"comment:'名称';not null" json:"name"`                                     // 名称
 	Code      string                `gorm:"comment:'编码';type:varchar(255);not null;uniqueIndex:udx_1" json:"code"` // 编码
 	IsEnabled bool                  `gorm:"comment:'是否启用';default:true" json:"is_enabled"`                         // 是否启用
