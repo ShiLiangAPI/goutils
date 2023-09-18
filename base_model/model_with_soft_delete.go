6432f2b37ac07d448991fa116b555fecc804518f
DELL <2509144896@qq.com>
2023-09-18 09:50:02 +0800

    [Mod] 删除唯一索引

--- base_model/model_with_soft_delete.go
+++ base_model/model_with_soft_delete.go
@@ -9,10 +9,10 @@ import (
 )
 
 type BaseModel struct {
-	ID        string                `gorm:"<-:create;type:bigint;primaryKey" json:"id"`                  // id
-	CreatedAt *types.LocalTime      `gorm:"comment:'创建时间'" json:"created_at"`                            // 创建时间
-	UpdatedAt *types.LocalTime      `gorm:"comment:'修改时间'" json:"updated_at"`                            // 修改时间
-	DeletedAt soft_delete.DeletedAt `gorm:"<-:create;softDelete:milli;index;uniqueIndex:udx_1" json:"-"` // 删除时间
+	ID        string                `gorm:"<-:create;type:bigint;primaryKey" json:"id"` // id
+	CreatedAt *types.LocalTime      `gorm:"comment:'创建时间'" json:"created_at"`           // 创建时间
+	UpdatedAt *types.LocalTime      `gorm:"comment:'修改时间'" json:"updated_at"`           // 修改时间
+	DeletedAt soft_delete.DeletedAt `gorm:"<-:create;softDelete:milli;index" json:"-"`  // 删除时间
 }
 
 func (obj *BaseModel) BeforeCreate(tx *gorm.DB) (err error) {
