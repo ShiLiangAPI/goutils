d641cfb75a042ac8b0b8c2de0f6a0ea6aae207da
reader <2509144896@qq.com>
2023-11-18 01:44:00 +0800

    [Add] 增加时间字段格式

---
+++ types/date_format.go
@@ -0,0 +1,41 @@
+package types
+
+import (
+	"database/sql/driver"
+	"fmt"
+	"time"
+)
+
+type LocalDate time.Time
+
+func (t *LocalDate) MarshalJSON() ([]byte, error) {
+	tTime := time.Time(*t)
+
+	return []byte(fmt.Sprintf("\"%v\"", tTime.Format("2006-01-02"))), nil
+}
+
+func (t LocalDate) Value() (driver.Value, error) {
+	var zeroTime time.Time
+	tlt := time.Time(t)
+
+	//判断给定时间是否和默认零时间的时间戳相同
+	if tlt.UnixNano() == zeroTime.UnixNano() {
+		return nil, nil
+	}
+
+	return tlt, nil
+}
+
+func (t *LocalDate) Scan(v any) error {
+	if value, ok := v.(time.Time); ok {
+		*t = LocalDate(value)
+		return nil
+	}
+
+	return fmt.Errorf("can not convert %v to timestamp", v)
+}
+
+func (t *LocalDate) String() string {
+
+	return fmt.Sprintf("hhh:%s", time.Time(*t).String())
+}
