63c3c5e8bd577bd5747391331e9d216d97db2dbf
DELL <2509144896@qq.com>
2024-01-08 19:54:26 +0800

    [Mod] 修改切片删除元素

--- function/slice_delete.go
+++ function/slice_delete.go
@@ -2,27 +2,33 @@ package function
 
 // SliceDeleteSelfValue list为基础数据，val为删除值
 // 效率更高，但修改了自己切片的值
-func SliceDeleteSelfValue[T comparable](list []T, val T) []T {
+func SliceDeleteSelfValue[T comparable](list []T, val T) ([]T, bool) {
 	j := 0
+	isExist := false
 	for _, v := range list {
 		if v != val {
 			list[j] = v
 			j++
+		} else {
+			isExist = true
 		}
 	}
 
-	return list[:j]
+	return list[:j], isExist
 }
 
 // SliceDeleteCopyValue list为基础数据，val为删除值
 // 效率没用上面的高，但不修改原切片
-func SliceDeleteCopyValue[T comparable](list []T, val T) []T {
+func SliceDeleteCopyValue[T comparable](list []T, val T) ([]T, bool) {
 	temp := make([]T, 0, len(list))
+	isExist := false
 	for _, v := range list {
 		if v != val {
 			temp = append(temp, val)
+		} else {
+			isExist = true
 		}
 	}
 
-	return temp
+	return temp, isExist
 }
